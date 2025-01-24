1918e1fa29f332a0862c8107a79175e1ce7143bc
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2025-01-24 21:43:46 +1000

    Add lesson 13 - embeddings

---
+++ 13-embeddings.ts
@@ -0,0 +1,51 @@
+import { createOpenAICompatible } from '@ai-sdk/openai-compatible'
+import { embedMany, embed, cosineSimilarity } from 'ai'
+
+const lmstudio = createOpenAICompatible({
+  name: 'lmstudio',
+  baseURL: 'http://localhost:1234/v1',
+})
+
+const model = lmstudio.textEmbeddingModel(
+  'text-embedding-nomic-embed-text-v1.5'
+)
+
+const values = ['Mars', 'Pineapple', 'Eucalyptus', 'Opinion']
+
+// Embeddings are an array of vectors, one for each word in `values`
+const { embeddings } = await embedMany({
+  model,
+  values,
+})
+
+// Store the embeddings in a basic vector database,
+// associating each value above with it's embedding
+const vectorDatabase = embeddings.map((embedding, index) => ({
+  value: values[index],
+  embedding,
+}))
+
+const searchTerm = await embed({
+  model,
+  value: 'Assumption',
+})
+
+const entries = vectorDatabase.map((entry) => {
+  return {
+    value: entry.value,
+    // Compare the search term with each entry to find similarity value, between 0 and 1
+    similarity: cosineSimilarity(entry.embedding, searchTerm.embedding),
+  }
+})
+
+// Sort by similarity, descending
+console.log(entries.sort((a, b) => b.similarity - a.similarity))
+
+/*
+[
+  { value: 'Opinion', similarity: 0.6168683508020734 },
+  { value: 'Eucalyptus', similarity: 0.47055352566035763 },
+  { value: 'Mars', similarity: 0.4543035286634257 },
+  { value: 'Pineapple', similarity: 0.4413786556235543 }
+]
+*/
