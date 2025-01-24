303e142075b174c3d2a7be18c0a207b60ad9f531
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2025-01-24 20:31:28 +1000

    Add lesson 10 - arrays

---
+++ 10-generate-arrays.ts
@@ -0,0 +1,36 @@
+import { createOpenAICompatible } from '@ai-sdk/openai-compatible'
+import { anthropic } from '@ai-sdk/anthropic'
+import { generateObject } from 'ai'
+import { z } from 'zod'
+
+const lmstudio = createOpenAICompatible({
+  name: 'lmstudio',
+  baseURL: 'http://localhost:1234/v1',
+})
+
+// Empty string defaults to the model selected in lmstudio
+const model = lmstudio('')
+// const model = anthropic('claude-3-5-haiku-latest')
+
+const schema = z.object({
+  name: z.string().describe(`The name of the user`),
+  age: z.number().describe(`The user's age`),
+  email: z.string().email().describe(`The user's email address`),
+  occupation: z.string().describe(`The user's occupation`),
+})
+
+export const createFakeUsers = async (prompt: string) => {
+  const result = await generateObject({
+    model,
+    prompt,
+    output: 'array', // array output
+    schema,
+    system: `You are generating fake user data.`,
+  })
+
+  return result.object
+}
+
+const fakeUsers = await createFakeUsers(`3 antarctic explorers`)
+
+console.log(fakeUsers)
