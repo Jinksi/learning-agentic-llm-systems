536c67c28651977218855dda6f344362be0c5a31
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2025-01-24 20:00:16 +1000

    Add lesson 9 – enums

---
+++ 09-generate-enums.ts
@@ -0,0 +1,30 @@
+import { createOpenAICompatible } from '@ai-sdk/openai-compatible'
+import { anthropic } from '@ai-sdk/anthropic'
+import { generateObject } from 'ai'
+
+const lmstudio = createOpenAICompatible({
+  name: 'lmstudio',
+  baseURL: 'http://localhost:1234/v1',
+})
+
+// Empty string defaults to the model selected in lmstudio
+const model = lmstudio('')
+// const model = anthropic('claude-3-5-haiku-latest')
+
+export const classifySentiment = async (text: string) => {
+  const { object } = await generateObject({
+    model,
+    output: 'enum',
+    enum: ['positive', 'neutral', 'negative'], // List our enums here.
+    prompt: text,
+    system:
+      `Classify the sentiment of the text as either ` +
+      `positive, negative, or neutral.`,
+  })
+
+  return object // This is not an object, but one of the values from the enum
+}
+
+console.log(await classifySentiment(`I'm not sure how I feel`)) // returns `neutral`
+console.log(await classifySentiment(`I'm pretty excited about this`)) // returns `positive`
+console.log(await classifySentiment(`I'm not excited at all`)) // returns `negative`
